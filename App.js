f4d19fa7d6ffc9efc95b690b2e228c0cf1b774d8
bigfudge99 <mayurnegi1999@gmail.com>
2022-08-24 01:01:20 +0530

    added eslint and prettier rules

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
 
-
 const App = () => {
   return (
     <SafeAreaView>
-      <StatusBar />
-      <Text>Hii</Text>
+		<StatusBar />
+		<Text>Hii</Text>
     </SafeAreaView>
   );
 };
